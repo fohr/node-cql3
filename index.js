2116a75bb29e3ddb3d7f97df446a9bd1c22e6a21
Isaac Wagner <isaacbwagner@gmail.com>
2013-02-15 12:22:26 -0500

    Created FrameBuilder

--- index.js
+++ index.js
@@ -2,14 +2,34 @@ var net = require('net');
 
 exports.Client = function(host, port, options) {
 
+    //a store of available stream IDs for new requests
     var streamIDs = [];
     for(var i = 0; i < 128; i++)
         streamIDs.push(i);
 
-    var streamCallbacks = {};
+    var replyCallbacks = {}; //TODO: queue requests if no stream IDs are available
+
+    var client; //socket
 
     this.connect = function(callback) {
+        function startup() {
+            
+        }
 
+        client = net.connect({ 
+            host: host,
+            port: port
+        }, function() {
+            startup();
+        });
+
+        client.on('end', function() {
+            client = null;
+        });
+
+        client.on('close', function() {
+            client = null;
+        });
     };
 
     this.query = function(query, callback) {
@@ -19,7 +39,6 @@ exports.Client = function(host, port, options) {
     this.prepare = function(query, callback) {
 
     };
-
 
     this.execute = function(id, values, consistency, callback) {
 
